3f39f4ba30238ab9b7a6dece4b5359318440c374
Eric Smith <esmith@8thlight.com>
2016-01-27 13:05:21 -0600

    Initial commit

---
+++ spec/javafx/core_spec.clj
@@ -0,0 +1,7 @@
+(ns javafx.core-spec
+  (:require [speclj.core :refer :all]
+            [javafx.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/javafx/core.clj
@@ -0,0 +1,1 @@
+(ns javafx.core)
