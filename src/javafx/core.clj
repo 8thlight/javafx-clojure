29323455d495c5c37523231196dcf1f1147db9ef
Eric Smith <esmith@8thlight.com>
2016-01-27 14:48:28 -0600

    Get a version that runs

--- src/javafx/core.clj
+++ src/javafx/core.clj
@@ -1,1 +1,13 @@
-(ns javafx.core)
+(ns javafx.core
+  (:import [javax.swing SwingUtilities JFrame JLabel]))
+
+(defn -main [& args]
+  (SwingUtilities/invokeLater
+    (proxy [Object Runnable] []
+      (run []
+        (let [label (JLabel. "Hello World")
+              frame (JFrame. "HelloWorldSwing")]
+          (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+          (.add (.getContentPane frame) label)
+          (.pack frame)
+          (.setVisible frame true))))))
